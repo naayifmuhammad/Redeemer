ba8748ae223713ed345d55043e096a6b9562e244
nikhilljpskj <nikhiljp.skj@gmail.com>
2024-08-20 00:39:38 +0530

    Post-revert

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import './Team.css';
 
 export const Team = (props) => {
   return (
-    <div id="team" className="text-center d-flex justify-content-center align-items-center">
+    <div id="team" className="text-center">
       <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-8 col-lg-12 section-title">
-            <h2>Meet the Team</h2>
-            <p style={{ textAlign: 'center' }}>
-              Leading Our Mission to Transform Your Digital Future
-            </p>
-          </div>
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Meet the Team</h2>
+          <p style={{ textAlign: 'center' }}>
+            Leading Our Mission to Transform Your Digital Future
+          </p>
         </div>
-        <div className="row justify-content-center">
-          <div id="team-grid">
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div key={`${d.name}-${i}`} className="team-member">
-                    <div className="thumbnail">
-                      <div className="img-container">
-                        <img src={d.img} alt="..." className="team-img" />
-                      </div>
-                      <div className="caption">
-                        <h4>{d.name}</h4>
-                        <p>{d.job}</p>
-                      </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    <div className="img-container">
+                      <img src={d.img} alt="..." className="team-img" />
+                    </div>
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
                     </div>
                   </div>
-                ))
-              : "loading"}
-          </div>
+                </div>
+              ))
+            : "loading"}
         </div>
       </div>
     </div>
